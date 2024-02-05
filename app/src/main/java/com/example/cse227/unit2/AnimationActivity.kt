788b304e6c66fca54c2f41606c1588047a472d9c
Amartya Sen <amartyasen895@gmail.com>
2024-02-05 12:54:19 +0530

    animation blink code unit-2

--- app/src/main/java/com/example/cse227/unit2/AnimationActivity.kt
+++ app/src/main/java/com/example/cse227/unit2/AnimationActivity.kt
@@ -2,8 +2,8 @@ package com.example.cse227.unit2
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.animation.AlphaAnimation
 import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +12,7 @@ import com.example.cse227.R
 class AnimationActivity : AppCompatActivity() {
     lateinit var  iv :ImageView
     lateinit var animeBtn : Button
+    lateinit var animBlink:Animation
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +23,15 @@ class AnimationActivity : AppCompatActivity() {
 
         animeBtn.setOnClickListener {
 
+            animBlink = AnimationUtils.loadAnimation(applicationContext , R.anim.blinkdemo)
+            iv.startAnimation(animBlink)
         }
-        val s = AlphaAnimation(0.0f,1.0f)
-        s.duration = 50
-        s.repeatMode = Animation.REVERSE
-        s.repeatCount = Animation.INFINITE
-        s.startOffset = 20
-
-        iv.startAnimation(s)
+//        val s = AlphaAnimation(0.0f,1.0f)
+//        s.duration = 50
+//        s.repeatMode = Animation.REVERSE
+//        s.repeatCount = Animation.INFINITE
+//        s.startOffset = 20
+//
+//        iv.startAnimation(s)
     }
 }
